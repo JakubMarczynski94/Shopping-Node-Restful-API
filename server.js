7641ab1cfaf63824e29897cf301f8ea55ce2e673
philljohnson <philljohnson739@gmail.com>
2022-08-22 02:07:12 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
